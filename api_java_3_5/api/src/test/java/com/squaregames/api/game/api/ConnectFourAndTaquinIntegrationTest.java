7ad47211ffab3a59eb9a3d788bf5e20b1797938d
SerenadeBird <SerenadeBird@proton.me>
2026-06-04 14:56:07 +0200

    style(test): supprime import inutilisé ParameterizedTypeReference

--- api_java_3_5/api/src/test/java/com/squaregames/api/game/api/ConnectFourAndTaquinIntegrationTest.java
+++ api_java_3_5/api/src/test/java/com/squaregames/api/game/api/ConnectFourAndTaquinIntegrationTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
